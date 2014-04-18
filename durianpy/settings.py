735a9f85b3fd8adc7a53dbb8daeab0912cbcda6a
Earvin Gemenez <earvin@ingenuity-tech.com>
2014-04-18 17:24:52 +0800

    heroku configuration

--- durianpy/settings.py
+++ durianpy/settings.py
@@ -65,14 +65,9 @@ WSGI_APPLICATION = 'durianpy.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'durianpy.sqlite3'),
-    }
-}
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
